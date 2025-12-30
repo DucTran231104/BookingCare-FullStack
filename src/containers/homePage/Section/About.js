94ad249726014fbcfd902ae529f5e5bdc4a5e4b0
TranDuc <tranthaitrongduc@gmail.com>
2025-12-30 15:44:54 +0700

    complete layout for homePage

--- src/containers/homePage/Section/About.js
+++ src/containers/homePage/Section/About.js
@@ -8,7 +8,30 @@ class About extends Component {
 
         return (
             <div className="section-share section-about" >
-                hello about
+                <div className="section-about-header" >
+                    Truyền thông nói về DukeTran & HuyHuy & KhaiTran
+                </div>
+                <div className="section-about-content" >
+                    <div className="content-left" >
+                        <iframe width="100%" height="400"
+                            src="https://www.youtube.com/embed/dQw4w9WgXcQ?list=RDdQw4w9WgXcQ"
+                            title="Rick Astley - Never Gonna Give You Up (Official Video) (4K Remaster)"
+                            frameborder="0"
+                            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                            referrerpolicy="strict-origin-when-cross-origin"
+                            allowfullscreen></iframe>
+                    </div>
+                    <div className="content-right" >
+                        <p>
+                            In conventional electronics, only the charge degree of freedom of an election is utilized to
+                            construct devices. The electron also possesses a spin angular momentum which is closely
+                            associated with its magnetic moment. The field of spintronics, or spin-based electronics, exploits
+                            both charge and spin degrees of freedom of electrons to provide additional functionalities to
+                            conventional electronic devices such as non-volatility and reduced power consumption.
+                        </p>
+
+                    </div>
+                </div>
             </div >
         );
     }
