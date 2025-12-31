107b3bad326968f75f53dc8c16b2e5cf43b39eb3
TranDuc <tranthaitrongduc@gmail.com>
2025-12-31 11:22:26 +0700

    design layout menu for admin page

--- src/containers/Header/menuApp.js
+++ src/containers/Header/menuApp.js
@@ -1,14 +1,47 @@
 export const adminMenu = [
-    { //hệ thống
-        name: 'menu.system.header', menus: [
+    { //quan ly nguoi dung
+        name: 'menu.admin.manage-users', menus: [
             {
-                name: 'menu.system.system-administrator.header',
-                subMenus: [
-                    { name: 'menu.system.system-administrator.user-manage', link: '/system/user-manage' },
-                    { name: 'menu.system.system-administrator.user-redux', link: '/system/user-redux' },
-                ]
+                name: 'menu.admin.crud', link: '/system/user-manage'
             },
-            // { name: 'menu.system.system-parameter.header', link: '/system/system-parameter' },
+            {
+                name: 'menu.admin.crud-redux', link: '/system/user-redux'
+            },
+            {
+                name: 'menu.admin.manage-doctors', link: '/system/doctor-manage'
+                // subMenus: [
+                //     { name: 'menu.system.system-administrator.user-manage', link: '/system/user-manage' },
+                //     { name: 'menu.system.system-administrator.user-redux', link: '/system/user-redux' },
+                // ]
+            },
+            {
+                name: 'menu.admin.manage-admin', link: '/system/admin-manage'
+            },
+
+        ]
+    },
+
+    { //quan ly phong kham
+        name: 'menu.admin.clinic', menus: [
+            {
+                name: 'menu.admin.manage-clinic', link: '/system/clinic-manage'
+            },
+        ]
+    },
+
+    { //quan ly chuyen khoa
+        name: 'menu.admin.specialty', menus: [
+            {
+                name: 'menu.admin.manage-specialty', link: '/system/specialty-manage'
+            },
+        ]
+    },
+
+    { //quan ly bai dang
+        name: 'menu.admin.handbook', menus: [
+            {
+                name: 'menu.admin.manage-handbook', link: '/system/handbook-manage'
+            },
         ]
     },
 ];
